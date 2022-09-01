1b5b652d2e580dff14ac54b4b7624722d970cae3
IntriguedSayan <sayan.mid590@gmail.com>
2022-09-01 15:16:19 +0530

    from terminal done

--- index2.js
+++ index2.js
@@ -1,7 +1,53 @@
-const fileModule=require("fs")
-function changeName(name,rename){
-    fileModule.rename(`./${name}`,`./${rename}`,(err)=>{
-        err?console.log(err):console.log("Rename has been completed");
-    })
+const args=process.argv
+
+const fs=require("fs")
+
+if(args[2]==="read"){
+    if(args.length>4) console.log("Please provide proper file name")
+    else{
+        fs.readFile(`./${args[3]}`,{encoding:"utf-8"},(err,data)=>{
+        
+            err?console.log(err):console.log(data)
+    
+        })
+    }
 }
-changeName("test2.txt","rename2.txt")
+else if(args[2]==="append"){
+    if(args.length>5) console.log("Please provide proper file name")
+    else{
+        const data=args[3]
+        fs.appendFile(`./${args[4]}`,data,(err)=>{
+            err?console.log(err):console.log("Append completed");
+        })
+    }
+}
+else if(args[2]==="delete"){
+    if(args.length>4) console.log("Please provide proper file name")
+    else{
+        fs.rm(`./${args[3]}`,(err)=>{
+            err?console.log(err):console.log("File has been deleted");
+        })
+    }
+}
+else if(args[2]==="create"){
+    if(args.length>4) console.log("Please provide proper file name")
+    else{
+        fs.createWriteStream(`${args[3]}`)
+    }
+}
+else if(args[2]==="rename"){
+    if(args.length>5) console.log("Please provide a proper file name")
+    else{
+        fs.rename(`./${args[3]}`,`./${args[4]}`,(err)=>{
+            err?console.log(err):console.log("Rename completed");
+        })
+    }
+}
+else if(args[2]==="list"){
+    if(args.length>3) console.log("Please provide proper file name")
+    else{
+        fs.readdir("./",(err,files)=>{
+            err?console.log(err):console.log(files)
+        })
+    }
+}
